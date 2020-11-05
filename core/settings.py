ef6315324dc527d32de5aa56f29a89f502f4d920
jaocfilho <augustoliveira95@outlook.com>
2020-11-05 02:34:22 -0300

    redis integrated and project finished

--- core/settings.py
+++ core/settings.py
@@ -152,3 +152,8 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_SEC
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
 }
+
+# Redis config
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
